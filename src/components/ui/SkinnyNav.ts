3817b302904de8282d4e88ec5bc7409ae503d4f6
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2025-01-25 16:40:13 +0100

    added feed and profile links to navigations

--- src/components/ui/SkinnyNav.ts
+++ src/components/ui/SkinnyNav.ts
@@ -20,6 +20,14 @@ const navLinks: NavLinksLabels = {
         textContent: 'Register',
         link: '/register/',
     },
+    feed: {
+        textContent: 'Feed',
+        link: '/feed/',
+    },
+    profile: {
+        textContent: 'Profile',
+        link: '/profile/',
+    },
 };
 
 class SkinnyNav extends HTMLElement {
@@ -32,7 +40,6 @@ class SkinnyNav extends HTMLElement {
 
     connectedCallback() {
         console.log('SkinnyNav connected yo!');
-        console.log(this.navLinks || 'what the heck?');
 
         this.addElementToDOM(this);
     }
@@ -40,18 +47,24 @@ class SkinnyNav extends HTMLElement {
     addElementToDOM(parent: HTMLElement) {
         const nav = document.createElement('ul');
         nav.role = 'navigation';
+        nav.className = 'group flex pad:space-x-chub space-x-skinny';
         parent.appendChild(nav);
         this.addLinksToNav(nav);
     }
 
     addLinksToNav(parent: HTMLElement) {
         for (const key in this.navLinks) {
-            const link = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = this.navLinks[key].link;
-            a.textContent = this.navLinks[key].textContent;
-            link.appendChild(a);
-            parent.appendChild(link);
+            const listItem = document.createElement('li');
+            const link = document.createElement('a');
+            link.href = this.navLinks[key].link;
+            link.textContent = this.navLinks[key].textContent;
+
+            listItem.className =
+                'pad:text-2xl text-tea_green-800 group-hover:text-tea_green-500 transition-all duration-300 hover:underline underline-offset-8 p-0';
+            link.className = 'py-skinny px-skinny';
+
+            listItem.appendChild(link);
+            parent.appendChild(listItem);
         }
     }
 }
