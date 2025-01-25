aa6ce350f8e3dde1e3cf414121581b97cd87368e
Kenneth Jahnsen Collins <106404582+kJAHsin@users.noreply.github.com>
2025-01-25 12:24:16 +0100

    removing boilerplate code from component

--- src/components/SkinnyHeader.ts
+++ src/components/SkinnyHeader.ts
@@ -1,22 +1,13 @@
 class SkinnyHeader extends HTMLElement {
-   constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
-      this.shadowRoot!.innerHTML = `
-         <style>
-            :host {
-               display: block;
-               background-color: #f8f9fa;
-               padding: 10px;
-               font-size: 1.2em;
-               text-align: center;
-            }
-         </style>
-         <div>
-            Skinny Header
-         </div>
-      `;
-   }
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    console.log("SkinnyHeader connected");
+  }
 }
 
-customElements.define('skinny-header', SkinnyHeader);
+customElements.define("skinny-header", SkinnyHeader);
